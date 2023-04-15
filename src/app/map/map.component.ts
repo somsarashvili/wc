d5cba44c9aa3b0d5556872fbbf2e291e287c7b11
Solo Omsarashvili <solo.omsarashvili@gmail.com>
2023-04-15 18:50:03 +0400

    test icons

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,10 @@
 import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import * as L from 'leaflet';
+import 'leaflet.awesome-markers/dist/leaflet.awesome-markers';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+library.add(faMapMarkerAlt);
 
 @Component({
   selector: 'app-map',
@@ -9,7 +14,10 @@ import * as L from 'leaflet';
 export class MapComponent implements OnInit, OnDestroy {
   @Output() map$: EventEmitter<L.Map> = new EventEmitter;
   @Output() zoom$: EventEmitter<number> = new EventEmitter;
-  
+
+  constructor() {
+  }
+
   map?: L.Map;
   zoom?: number;
   options: L.MapOptions = {
@@ -17,12 +25,28 @@ export class MapComponent implements OnInit, OnDestroy {
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         opacity: 0.7,
         maxZoom: 19,
-        detectRetina: true,
+        detectRetina: false,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       })],
     zoom: 15,
     center: L.latLng(41.730811, 44.779581)
   }
+  greenIcon = L.icon({
+    iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-green.png',
+    shadowUrl: 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png',
+
+    iconSize: [38, 95], // size of the icon
+    shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
+  markers: L.Marker[] = [
+    L.marker([41.730811, 44.779581], {icon: this.greenIcon}).bindPopup('First Pin'),
+    L.marker([41.7261055606886, 44.74963733728571], {icon: this.greenIcon}).bindPopup('Second Pin'),
+    L.marker([41.72982084314337, 44.74620410987881], {icon: this.greenIcon}).bindPopup('Third Pin')
+  ];
+
 
   ngOnInit() {
   }
@@ -32,10 +56,17 @@ export class MapComponent implements OnInit, OnDestroy {
     this.map$.emit(map);
     this.zoom = map.getZoom();
     this.zoom$.emit(this.zoom);
+    this.addMarkers();
   }
 
   ngOnDestroy() {
     this.map?.clearAllEventListeners;
     this.map?.remove();
   };
+
+  addMarkers() {
+    this.markers.forEach(marker => {
+      marker.addTo(this.map!);
+    });
+  }
 }
